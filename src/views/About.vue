8da9e77ba77dd85b0e686ce5a595007d9ec1ddb0
Pietro <pietro.tambu2003@gmail.com>
2021-03-25 16:15:20 +0100

    .

--- src/views/About.vue
+++ src/views/About.vue
@@ -28,11 +28,11 @@ export default {
   width: 65vw;
   max-width: 600px;
   margin: auto;
+  margin-top: 30px;
   border: 2px black solid;
   border-radius: 15px;
   box-shadow: 0 0 10px #000;
   background-color: rgba(255, 255, 255, 0.5);
-  margin-top: 30px;
   padding: 20px;
 }
 
