450c18b7b6be9b4bcd58da33f62a640735423b6e
Pietro <pietro.tambu2003@gmail.com>
2021-03-27 09:02:03 +0100

    27-03-2021 09:02

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,72 +1,30 @@
 <template>
   <div class="about">
-    <div class="div-main">
-      <h2>City Pollution Tambu</h2>
-      <img src="../assets/favicon.png" alt="logo" width="150" height="150" style="margin: 10px">
-      <p style="font-family: JosefinSans, monospace"><Strong>Codemotion 2021</Strong> project by Pietro Tamburini <strong>Milano</strong></p>
-      <hr>
-      <h3>Source Code:</h3>
-      <p> GitHub:
-        <a href="https://github.com/PietroTambu/city-pollution-codemotion" target="_blank">City Pollution Codemotion </a>
-        <img src="../assets/github.png" alt="GitHub Logo" width="50" height="50" style="margin-left: 10px">
-      </p>
-      <hr>
-      <!--
-      <h3>Utilities:</h3>
-      <p>
-        <a href="https://firebase.google.com/" target="_blank">Google Firebase</a>
-        <img src="../assets/firebase.png" alt="Firebase Logo" width="50" height="50" style="margin-left: 10px">
-      </p>
-      <p>
-        <a href="https://stackoverflow.com/" target="_blank">Stack Overflow</a>
-        <img src="../assets/stackoverflow.png" alt="Stack Overflow Logo" width="70" height="70" style="margin-left: 10px">
-      </p>
-      <p>
-        <a href="https://vuejs.org/" target="_blank">Vue JS framework</a>
-        <img src="../assets/vue-js.png" alt="Vue JS Logo" width="50" height="50" style="margin-left: 10px">
-      </p>
-      <p>
-        <a href="https://aqicn.org/map/world/" target="_blank">World Air Quality Index "AQICN"</a>
-        <img src="../assets/aqicn-logo.png" alt="AQICN Logo" width="50" height="50" style="margin-left: 10px">
-      </p>
-      <hr>
-      <b-container fluid="sm" class="text-center">
-        <b-row>
-          <b-col class="bg-dark text-light rounded-left border border-secondary">Name</b-col>
-          <b-col class="bg-dark text-light border border-secondary">Utility</b-col>
-          <b-col class="bg-dark text-light rounded-right border border-secondary">Icon</b-col>
-        </b-row>
-        <b-row class="text-center">
-          <b-col class="bg-light text-dark rounded-left border border-secondary" align-self="center"><p class="table-p">Google Firebase</p></b-col>
-          <b-col class="bg-light text-dark border border-secondary"><p class="table-p">Hosting site</p></b-col>
-          <b-col class="bg-light text-dark rounded-right border border-secondary">
-            <img src="../assets/firebase.png" alt="Google Firebase" width="50" height="50">
-          </b-col>
-        </b-row>
-        <b-row class="text-center">
-          <b-col class="bg-light text-dark rounded-left border border-secondary" align-self="center"><p class="table-p">Stack Overflow</p></b-col>
-          <b-col class="bg-light text-dark border border-secondary"><p class="table-p">Learn notions</p></b-col>
-          <b-col class="bg-light text-dark rounded-right border border-secondary">
-            <img src="../assets/stackoverflow.png" alt="Google Firebase" width="50" height="50">
-          </b-col>
-        </b-row>
-      </b-container>
-      <hr>
-      -->
-      <b-table small :fields="fields" :items="items" responsive="sm" :dark="dark" :bordered="bordered">
-        <template #cell(name)="data">
-          <b class="text-dark"><p :href="data.value.url" class="table-p" target="_blank">{{ data.value.name }}</p></b>
-        </template>
-
-        <template #cell(utility)="data">
-          <b class="text-dark"><p class="table-p">{{ data.value }}</p></b>
-        </template>
-
-        <template #cell(icon)="data">
-          <img v-bind:src="data.value" alt="ok" width="50" height="50">
-        </template>
-      </b-table>
-    </div>
+    <b-overlay :show="showMain" rounded="sm" variant="secondary" opacity="0.85" blur="2px" style="width: fit-content; margin: auto">
+      <div class="div-main">
+        <h2>City Pollution Tambu</h2>
+        <img src="../assets/favicon.png" alt="logo" width="150" height="150" style="margin: 10px">
+        <p style="font-family: JosefinSans, monospace"><Strong>Codemotion 2021</Strong> project by Pietro Tamburini <strong>Milano</strong></p>
+        <hr>
+        <h3>Source Code:</h3>
+        <p> GitHub:
+          <a href="https://github.com/PietroTambu/city-pollution-codemotion" target="_blank">City Pollution Codemotion </a>
+          <img src="../assets/github.png" alt="GitHub Logo" width="50" height="50" style="margin-left: 10px">
+        </p>
+        <hr>
+        <b-table small :fields="fields" :items="items" responsive="sm" :dark="dark" :bordered="bordered">
+          <template #cell(name)="data">
+            <b class="text-dark"><p :href="data.value.url" class="table-p" target="_blank">{{ data.value.name }}</p></b>
+          </template>
+          <template #cell(utility)="data">
+            <b class="text-dark"><p class="table-p">{{ data.value }}</p></b>
+          </template>
+          <template #cell(icon)="data">
+            <img v-bind:src="data.value" alt="ok" width="50" height="50">
+          </template>
+        </b-table>
+      </div>
+    </b-overlay>
   </div>
 </template>
 
@@ -77,17 +35,24 @@ export default {
     return {
       fields: ['name', 'utility', 'icon'],
       items: [
-        { name: { name: 'Google Firebase', url: 'https://firebase.google.com/' }, utility: 'Hosting', icon: '/img/firebase.a6c26916.png', _rowVariant: 'light' },
-        { name: { name: 'Stack Overflow', url: 'https://stackoverflow.com/' }, utility: 'Learn notions', icon: '/img/stackoverflow.5233a552.png', _rowVariant: 'light' },
-        { name: { name: 'Vue.js Framework', url: 'https://vuejs.org/' }, utility: 'Platform Framework', icon: '/img/vue-js.96e5850d.png', _rowVariant: 'light' },
-        { name: { name: 'World Air Quality Index', url: 'https://aqicn.org/map/world/' }, utility: 'Receive data', icon: '/img/aqicn-logo.41bab77c.png', _rowVariant: 'light' }
+        { name: { name: 'Google Firebase', url: 'https://firebase.google.com/' }, utility: 'Hosting Service Provider', icon: '../assets/firebase.png', _rowVariant: 'light' },
+        { name: { name: 'Stack Overflow', url: 'https://stackoverflow.com/' }, utility: 'Learn Knowledge', icon: '../assets/stackoverflow.png', _rowVariant: 'light' },
+        { name: { name: 'Vue.js Framework', url: 'https://vuejs.org/' }, utility: 'Development Platform', icon: '../assets/vue-js.png', _rowVariant: 'light' },
+        { name: { name: 'World Air Quality Index', url: 'https://aqicn.org/map/world/' }, utility: 'Data Provider', icon: '../assets/aqicn-logo.png', _rowVariant: 'light' }
       ],
       tableVariants: ['primary', 'secondary', 'info', 'danger', 'warning', 'success', 'light', 'dark'],
       dark: true,
-      bordered: true
+      bordered: true,
+      showMain: true
     }
   },
   methods: {
+  },
+  mounted () {
+    setTimeout(
+      () => {
+        this.showMain = !this.showMain
+      }, 600)
   }
 }
 
@@ -108,7 +73,7 @@ export default {
   border: 2px black solid;
   border-radius: 15px;
   box-shadow: 0 0 10px #000;
-  background-color: rgba(255, 255, 255, 0.6);
+  background-color: rgba(255, 255, 255, 0.5);
   padding: 20px;
 }
 
